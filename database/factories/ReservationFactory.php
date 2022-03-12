cc04b7cbf569bde9275020b5830ee498169cb36d
Uğur Arıcı <m.ugurarici@gmail.com>
2022-03-12 12:43:43 +0300

    Update ReservationFactory to use a random user randomly

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
@@ -19,6 +20,7 @@ class ReservationFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'user_id' => rand(0, 9) > 4 ? User::inRandomOrder()->first()->id : null,
             'email' => $this->faker->safeEmail(),
             'phone' => $this->faker->phoneNumber(),
             'reservation_at' => (new Carbon($this->faker->dateTimeBetween('now', '+1 week')))->startOfDay()->addHours(rand(10, 17)),
